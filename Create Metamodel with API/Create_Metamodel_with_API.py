a55f985619181b38547f0756a1d50342432d4599
semen.yakushev@resheniya.org <semen.yakushev@resheniya.org>
2021-12-06 18:43:19 +0300

    version with reltype

--- Create Metamodel with API/Create_Metamodel_with_API.py
+++ Create Metamodel with API/Create_Metamodel_with_API.py
@@ -8,35 +8,34 @@ import pandas as pd
 
 auth_key = 'c3lzdGVtIGFkbWluaXN0cmF0b3I6MTIzcXdlQVNEKw=='
 base_url = 'http://185.53.22.117:8008'
-file = "export.xlsx"
+file = "Archimate-Metamodel перевод.xlsx"
 
+relations = pd.read_excel(io=file, header = 0, engine='openpyxl', sheet_name = "Mapping")
+print(relations)
 
-objects = pd.read_excel(io=file, header = 0, engine='openpyxl', squeeze = True)
-print(objects)
-
-list_types = []
-
+rel_list_types = []
 headers = {
-    'Accept': 'application/json',
-    'Content-Type': 'application/json', 
-    'Authorization': 'Basic ' + auth_key,
+           'Accept': 'application/json',
+           'Content-Type': 'application/json', 
+           'Authorization': 'Basic ' + auth_key
 }
-
-for items in objects:
-   list_types.append({'Name': items})
-   
-for type in list_types:
-   jdata = json.dumps(type)
-   response = requests.post(base_url + '/api/metaModel/objectType', headers=headers, data=jdata)
 
 #class APIGET:
     #def get_objecttypes(self, auth_key, base_url):
-       #pass
+       #objects = pd.read_excel(io=file, header = 0, engine='openpyxl', sheet_name = "ObjectTypes")
+       #obj_list_types = [] 
+       #headers = {
+           #'Accept': 'application/json',
+           #'Content-Type': 'application/json', 
+           #'Authorization': 'Basic ' + auth_key
+           #}
+       #for items in objects:
+           #obj_list_types.append({'Name': items})
+           #for type in obj_list_types:
+               #jdata = json.dumps(type)
+               #response = requests.post(base_url + '/api/metaModel/objectType', headers=headers, data=jdata)
 
     #def get_relationshipstypes(self, auth_key, base_ur):
-        #pass
-
-    #def get_relationships_pairs(self, auth_key, base_ur):
         #pass
 
     #def get_atributestype(self, auth_key, base_ur):
