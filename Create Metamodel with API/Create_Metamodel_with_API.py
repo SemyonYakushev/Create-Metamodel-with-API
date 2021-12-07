562638a8830e6afd7cdf6685ea88897d65f1b08c
Семён Якушев <semen.yakushev@resheniya.org>
2021-12-07 15:17:38 +0300

    obj and rel done

--- Create Metamodel with API/Create_Metamodel_with_API.py
+++ Create Metamodel with API/Create_Metamodel_with_API.py
@@ -10,43 +10,41 @@ auth_key = 'c3lzdGVtIGFkbWluaXN0cmF0b3I6MTIzcXdlQVNEKw=='
 base_url = 'http://185.53.22.117:8009'
 file = "Archimate-Metamodel перевод.xlsx"
 
-columns = ['RelationTypeName', 'FromObjectTypeName', 'ToObjectTypeName']
-relations = pd.read_excel(io=file, header = 0, engine='openpyxl', sheet_name = "Mapping")
-#print(relations[columns])
-
-
-rel_list_types = []
-headers = {
+class iServerMetamodel(object):
+    def get_objecttypes(self, auth_key, base_url):
+       objects = pd.read_excel(io=file, header = 0, engine='openpyxl', sheet_name = "ObjectTypes") 
+       headers = {
            'Accept': 'application/json',
            'Content-Type': 'application/json', 
            'Authorization': 'Basic ' + auth_key
-}
+           }
+ 
+       for index, item in objects.iterrows():
+           obj_json = {'Name': item['RusTypeName'], 'Description': item['ObjectTypeName']}
+           jdata = json.dumps(obj_json)
+           response = requests.post(base_url + '/api/metaModel/objectType', headers=headers, data=jdata)
+           print(response.text)
 
-for rows in relations[columns]:
-    #a = {'Name': items['RelationTypeName']}
-    #rel_list_types.append(a)
-    print(rows)
+    def get_relationshipstypes(self, auth_key, base_url):
+        columns = ['RelationTypeName', 'FromObjectTypeName', 'ToObjectTypeName']
+        relations = pd.read_excel(io=file, header = 0, engine='openpyxl', sheet_name = "Mapping")
+        headers = {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json', 
+                'Authorization': 'Basic ' + auth_key
+                }
 
-#class APIGET:
-    #def get_objecttypes(self, auth_key, base_url):
-       #objects = pd.read_excel(io=file, header = 0, engine='openpyxl', sheet_name = "ObjectTypes")
-       #obj_list_types = [] 
-       #headers = {
-           #'Accept': 'application/json',
-           #'Content-Type': 'application/json', 
-           #'Authorization': 'Basic ' + auth_key
-           #}
-       #for rows in objects:
-           #obj_list_types.append({'Name': rows})
-           #for type in obj_list_types:
-               #jdata = json.dumps(type)
-               #response = requests.post(base_url + '/api/metaModel/objectType', headers=headers, data=jdata)
+        for index, item in relations.iterrows():
+            rel_json = {'Name': item['RelationTypeName'],
+                        'Rules': [
+                            {'SourceType': {'Type': item['FromObjectTypeName']},
+                            'TargetType': {'Type': item['ToObjectTypeName']}}], 'SourceToTargetDescription': item['SourceToTarget'], 'TargetToSourceDescription': item['TargetToSource']}    
+            jdata = json.dumps(rel_json)
+            response = requests.post(base_url + '/api/metaModel/relationshipType', headers=headers, data=jdata)
+            print(response.text)
 
-    #def get_relationshipstypes(self, auth_key, base_ur):
+    #def get_atributestype(self, auth_key, base_url):
         #pass
 
-    #def get_atributestype(self, auth_key, base_ur):
-        #pass
-
-    #def get_atributes_assignment(self, auth_key, base_ur):
+    #def get_atributes_assignment(self, auth_key, base_url):
         #pass
