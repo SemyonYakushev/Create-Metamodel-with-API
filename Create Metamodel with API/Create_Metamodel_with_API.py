87fea848fae05f5c6e37b67e2ad94e66404b7334
Семён Якушев <semen.yakushev@resheniya.org>
2021-12-08 14:42:36 +0300

    add attributeType

--- Create Metamodel with API/Create_Metamodel_with_API.py
+++ Create Metamodel with API/Create_Metamodel_with_API.py
@@ -25,7 +25,7 @@ class iServerMetamodel:
            response = requests.post(base_url + '/api/metaModel/objectType', headers=headers, data=jdata)
            print(response.text)
 
-    def post_relationshipstypes(auth_key, base_url, file):
+    def post_relationshiptypes(auth_key, base_url, file):
         columns = ['RelationTypeName', 'FromObjectTypeName', 'ToObjectTypeName']
         relations = pd.read_excel(io=file, header = 0, engine='openpyxl', sheet_name = "Mapping")
         headers = {
@@ -33,21 +33,74 @@ class iServerMetamodel:
                 'Content-Type': 'application/json', 
                 'Authorization': 'Basic ' + auth_key
                 }
-
+        rel_json = {}
+        relType = ""
         for index, item in relations.iterrows():
-            rel_json = {'Name': item['RelationTypeName'],
+            if relType != item['RelationTypeName']:
+                if len(rel_json) > 0:
+                     jdata = json.dumps(rel_json)
+                     response = requests.post(base_url + '/api/metaModel/relationshipType', headers=headers, data=jdata)
+                     print(rel_json)
+                     print('------------------------------------------')
+                rel_json = {'Name': item['RelationTypeName'],
                         'Rules': [
-                            {'SourceType': {'Type': item['FromObjectTypeName']},
-                            'TargetType': {'Type': item['ToObjectTypeName']}}], 'SourceToTargetDescription': item['SourceToTarget'], 'TargetToSourceDescription': item['TargetToSource']}    
-            jdata = json.dumps(rel_json)
-            response = requests.post(base_url + '/api/metaModel/relationshipType', headers=headers, data=jdata)
-            print(response.text)
+                            {
+                            'SourceType': {'Type': item['FromObjectTypeName']},
+                            'TargetType': {'Type': item['ToObjectTypeName']}
+                            }
+                            ],
+                            'SourceToTargetDescription': item['SourceToTarget'], 
+                            'TargetToSourceDescription': item['TargetToSource'],
+                            'HasDirection': 'true'
+                            }
+                relType = item['RelationTypeName']
+               
+            else:
+                rel_json['Rules'].append(
+                            {
+                            'SourceType': {'Type': item['FromObjectTypeName']},
+                            'TargetType': {'Type': item['ToObjectTypeName']}
+                            })
+               
+        jdata = json.dumps(rel_json)
+        response = requests.post(base_url + '/api/metaModel/relationshipType', headers=headers, data=jdata)
 
-    #def get_atributestype(self, auth_key, base_url):
-        #pass
+
+            
+
+    def post_attributetypes(auth_key, base_url, file):
+       attributes = pd.read_excel(io=file, header = 0, engine='openpyxl', sheet_name = "Attribute") 
+       headers = {
+           'Accept': 'application/json',
+           'Content-Type': 'application/json', 
+           'Authorization': 'Basic ' + auth_key
+           }
+ 
+       for index, item in attributes.iterrows():
+          atr_json = {
+          'Name': item['RusAttrName'],
+          'DataType':  item['AttributeType'],
+          'IsMandatory': 'false',
+          'SyncWithVisio': item['IsSynchronised'],
+          'VisioSyncName': item['VisioSyncName'],
+          'ListValues':  [
+            {
+          'Name': 'test'
+            }]
+          }
+          if item['AttributeType'] == 'List':
+              atr_json['ListValues'].append(
+                {
+                  'Name': 'test2'
+                })
+       #jdata = json.dumps(obj_json)
+       #response = requests.post(base_url + '/api/metaModel/attributeType', headers=headers, data=jdata)
+       print(atr_json)
 
     #def get_atributes_assignment(self, auth_key, base_url):
         #pass
 
 
-iServerMetamodel.post_objecttypes(auth_key = auth_key, base_url = base_url, file=file)
+#iServerMetamodel.post_objecttypes(auth_key = auth_key, base_url = base_url, file=file)
+#iServerMetamodel.post_relationshiptypes(auth_key = auth_key, base_url = base_url, file=file)
+iServerMetamodel.post_attributetypes(auth_key = auth_key, base_url = base_url, file=file)
