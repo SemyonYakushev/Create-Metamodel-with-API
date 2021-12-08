156c47365fe31dca1f893118fd567dd83b19d057
Семён Якушев <semen.yakushev@resheniya.org>
2021-12-08 11:45:49 +0300

    object done

--- Create Metamodel with API/Create_Metamodel_with_API.py
+++ Create Metamodel with API/Create_Metamodel_with_API.py
@@ -10,8 +10,8 @@ auth_key = 'c3lzdGVtIGFkbWluaXN0cmF0b3I6MTIzcXdlQVNEKw=='
 base_url = 'http://185.53.22.117:8009'
 file = "Archimate-Metamodel перевод.xlsx"
 
-class iServerMetamodel(object):
-    def get_objecttypes(self, auth_key, base_url):
+class iServerMetamodel:
+    def post_objecttypes(auth_key, base_url, file):
        objects = pd.read_excel(io=file, header = 0, engine='openpyxl', sheet_name = "ObjectTypes") 
        headers = {
            'Accept': 'application/json',
@@ -25,7 +25,7 @@ class iServerMetamodel(object):
            response = requests.post(base_url + '/api/metaModel/objectType', headers=headers, data=jdata)
            print(response.text)
 
-    def get_relationshipstypes(self, auth_key, base_url):
+    def post_relationshipstypes(auth_key, base_url, file):
         columns = ['RelationTypeName', 'FromObjectTypeName', 'ToObjectTypeName']
         relations = pd.read_excel(io=file, header = 0, engine='openpyxl', sheet_name = "Mapping")
         headers = {
@@ -48,3 +48,6 @@ class iServerMetamodel(object):
 
     #def get_atributes_assignment(self, auth_key, base_url):
         #pass
+
+
+iServerMetamodel.post_objecttypes(auth_key = auth_key, base_url = base_url, file=file)
